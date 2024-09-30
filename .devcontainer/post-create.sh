5b953e85e532c03454e7e19bb1e775970fb121ba
Adam Gardner <adam.gardner@dynatrace.com>
2024-09-30 14:51:12 +1000

    update

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,10 +6,10 @@ kind create cluster --config .devcontainer/kind-cluster.yml --wait 300s
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
+# TODO: Install OneAgent
+
 # Install MariaDB
 helm install unguard-mariadb bitnami/mariadb --version 11.5.7 --set primary.persistence.enabled=false --wait --namespace unguard --create-namespace
 
 # Install Unguard
 helm install unguard  oci://ghcr.io/dynatrace-oss/unguard/chart/unguard --wait --namespace unguard --create-namespace
-
-# TODO: Install OneAgent
