15db983d095fa6f548c407bcc744ee25d886d8b9
Adam Gardner <adam.gardner@dynatrace.com>
2025-08-04 10:18:15 +1000

    add feedback box

--- docs/snippets/bizevent-access-user-interface.js
+++ docs/snippets/bizevent-access-user-interface.js
@@ -1,5 +1,7 @@
 <script>
+var CURRENT_PAGE = "4. access-user-interface";
+
 document.addEventListener('DOMContentLoaded', function() {
-  dynatrace.sendBizEvent('page_load', {"page": "4. access-user-interface"});
+  dynatrace.sendBizEvent('page_load', {"page": CURRENT_PAGE});
 });
 </script>
--- docs/snippets/bizevent-log4shell-jndi.js
+++ docs/snippets/bizevent-log4shell-jndi.js
@@ -1,5 +1,7 @@
 <script>
+var CURRENT_PAGE = "7c. log4shell-jndi";
+
 document.addEventListener('DOMContentLoaded', function() {
-  dynatrace.sendBizEvent('page_load', {"page": "7c. log4shell-jndi"});
+  dynatrace.sendBizEvent('page_load', {"page": CURRENT_PAGE});
 });
 </script>
